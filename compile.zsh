8f1ca5598326928e483f88b275f206183a9dc5e5
Allemand Instable <68656923+code-instable@users.noreply.github.com>
2025-03-21 17:10:41 +0500

    build within `build` directory

--- compile.zsh
+++ compile.zsh
@@ -1,17 +1,18 @@
+mkdir -p build
 # Windows amd64
-GOOS=windows GOARCH=amd64 go build -o lutf-windows-amd64
+GOOS=windows GOARCH=amd64 go build -o build/lutf-windows-amd64
 
 # Windows arm64
-GOOS=windows GOARCH=arm64 go build -o lutf-windows-arm64
+GOOS=windows GOARCH=arm64 go build -o build/lutf-windows-arm64
 
 # macOS amd64
-GOOS=darwin GOARCH=amd64 go build -o lutf-darwin-amd64
+GOOS=darwin GOARCH=amd64 go build -o build/lutf-darwin-amd64
 
 # macOS arm64
-GOOS=darwin GOARCH=arm64 go build -o lutf-darwin-arm64
+GOOS=darwin GOARCH=arm64 go build -o build/lutf-darwin-arm64
 
 # Linux amd64
-GOOS=linux GOARCH=amd64 go build -o lutf-linux-amd64
+GOOS=linux GOARCH=amd64 go build -o build/lutf-linux-amd64
 
 # Linux arm64
-GOOS=linux GOARCH=arm64 go build -o lutf-linux-arm64
+GOOS=linux GOARCH=arm64 go build -o build/lutf-linux-arm64
