50399779624afdc9b80fdd14b707877b48c1fc20
Allemand Instable <68656923+code-instable@users.noreply.github.com>
2025-03-22 21:12:41 +0100

    Update compile.zsh

--- compile.zsh
+++ compile.zsh
@@ -1,18 +1,25 @@
 mkdir -p build
+
 # Windows amd64
+echo "➀ compiling for windows amd64..."
 GOOS=windows GOARCH=amd64 go build -o build/lutf-windows-amd64
 
 # Windows arm64
+echo "➁ compiling for windows arm64..."
 GOOS=windows GOARCH=arm64 go build -o build/lutf-windows-arm64
 
 # macOS amd64
+echo "➂ compiling for macos amd64..."
 GOOS=darwin GOARCH=amd64 go build -o build/lutf-darwin-amd64
 
 # macOS arm64
+echo "➃ compiling for macos arm64..."
 GOOS=darwin GOARCH=arm64 go build -o build/lutf-darwin-arm64
 
 # Linux amd64
+echo "➄ compiling for linux amd64..."
 GOOS=linux GOARCH=amd64 go build -o build/lutf-linux-amd64
 
 # Linux arm64
+echo "➅ compiling for linux arm64..."
 GOOS=linux GOARCH=arm64 go build -o build/lutf-linux-arm64
